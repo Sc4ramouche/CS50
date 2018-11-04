94d6da560571b0d23882ccdb3f80e871b1460ac3
Vladislav Kovechenkov <Vladislav.Kovechenkov@gmail.com>
2018-11-04 14:54:45 +0300

    Succint Fibonacci recursive function structure

--- Fibonacci/fibonacci.c
+++ Fibonacci/fibonacci.c
@@ -19,13 +19,10 @@ int main(int argc, string argv[])
 
 int fibonacci(int n)
 {
-    if (n == 0 || n == 1)
-    {
+    if (n == 1)
         return 1;
-    }
-    else
-    {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
-    return 1;
+    if (n == 0)
+        return 0;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
